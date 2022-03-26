02810d37f01a81f34858831d890751a7f0208530
Wh1teG17 <88611183+Wh1teG17@users.noreply.github.com>
2022-03-26 13:12:50 +0300

    Add files via upload

---
+++ les1/Course.java
@@ -0,0 +1,13 @@
+package les1;
+
+public class Course {
+    public int runDistance;
+    public int swimDistance;
+    public int jumpHeight;
+
+    public Course(int runDistance, int swimDistance, int jumpHeight) {
+        this.runDistance = runDistance;
+        this.swimDistance = swimDistance;
+        this.jumpHeight = jumpHeight;
+    }
+}
---
+++ les1/ICompetitor.java
@@ -0,0 +1,7 @@
+package les1;
+
+public interface ICompetitor {
+    String run(Course course);
+    String swim(Course course);
+    String jump(Course course);
+}
