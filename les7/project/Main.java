c75e8b7feddf2687c1cbaf67bbd8d762ab6d9202
Wh1teG17 <88611183+Wh1teG17@users.noreply.github.com>
2022-04-15 19:42:47 +0300

    home work 7

---
+++ les7/project/Main.java
@@ -0,0 +1,9 @@
+package les7.project;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+
+        userInterfaceView.runInterface();
+    }
+}
---
+++ les7/project/Period.java
@@ -0,0 +1,5 @@
+package les7.project;
+
+public enum Period {
+    NOW, FIVE_DAYS, DB
+}
